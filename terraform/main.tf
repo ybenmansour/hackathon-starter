5a010373baaba6a08200bcddd40fdedb330c5b4c
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-02 14:47:30 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,64 +12,10 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-data "aws_ami" "ubuntu" {
-   most_recent = true
-
-   filter {
-     name   = "name"
-     values = ["ubuntu/images/*ubuntu-bionic-18.04-amd64-server-*"]
-   }
-
-   filter {
-     name   = "virtualization-type"
-     values = ["hvm"]
-   }
-
-   filter {
-     name   = "root-device-type"
-     values = ["ebs"]
-   }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_security_group" "jenkins_group" {
-  name        = "allow_jenkins_conection"
-  description = "Allowjenkins port inbound traffic"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0fe5fc14a9ac91050"
   instance_type = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.jenkins_group.id]
+  vpc_security_group_ids = "sg-090df9b3d5604bae1"
 
 
 }
