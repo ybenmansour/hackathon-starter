9d0c6fef5ca9bbfc46006d0be48ab644dd101bf2
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-02 15:00:12 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,14 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+data "aws_security_group" "selected" {
+  id ="sg-000312648cb099634"
+}
+
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0fe5fc14a9ac91050"
   instance_type = "t2.medium"
-  vpc_security_group_ids = "sg-090df9b3d5604bae1"
+  vpc_security_group_ids = [aws_security_group.jenkins_server.id]
 
 
 }
