653f3baecbe4151a1ad33f2c99642cf85ea2173b
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-11 18:20:57 +0200

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,8 +2,3 @@ output "ec2_jenkins_server" {
   description = "IP of the Jenkins Server instance"
   value = aws_instance.jenkins_server.public_ip
 }
-
-output "ec2_jenkins_instance" {
-  description = "ID of the EC2 instance"
-  value = aws_instance.jenkins_server.id
-}
