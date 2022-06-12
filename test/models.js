e778001270e6ad9fe3a501f2de9f271e660dc96b
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-12 16:29:04 +0200

    Update models.js

--- test/models.js
+++ test/models.js
@@ -102,21 +102,6 @@ describe('User Model', () => {
     });
   });
 
-  it('should check password', (done) => {
-    const UserMock = sinon.mock(new User({
-      email: 'test@gmail.com',
-      password: '$2b$10$LhjJj5s1pLY/I4eCRaHaB.Fli8NBT8z1L8YF4/pmVU.5pERg4Z1AC'
-    }));
-
-    const user = UserMock.object;
-
-    user.comparePassword('root', (err, isMatched) => {
-      expect(err).to.equal(undefined);
-      expect(isMatched).to.equal(true);
-      done();
-    });
-  });
-
   it('should generate gravatar without email and size', () => {
     const UserMock = sinon.mock(new User({}));
     const user = UserMock.object;
