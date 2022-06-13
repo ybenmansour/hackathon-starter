333a959573215279c212e4cdcf823aba1d51e030
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-13 08:53:34 +0200

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://localhost:9000',
+        serverUrl:  'http://sonarqube:9000',
         options : {
             'sonar.sources':  '.',
             'sonar.tests':  'src',
