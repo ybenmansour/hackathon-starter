b51351c20cd0590630250e66165855bc1baf5ebd
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-17 19:28:43 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,10 @@ data "aws_security_group" "jenkins_server_group" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-0bf36a83943035310"
-  instance_type = "t2.medium"
+  ami           = "ami-095f3c8ea0f16631b"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.jenkins_server_group.id]
-  instance_initiated_shutdown_behavior = "terminate"
+  #instance_initiated_shutdown_behavior = "terminate"
 
 
 }
