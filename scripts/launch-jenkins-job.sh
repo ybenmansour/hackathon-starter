a5738e51985baae441d456eb3a4563425723b5d2
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-18 12:06:35 +0200

    Update launch-jenkins-job.sh

--- scripts/launch-jenkins-job.sh
+++ scripts/launch-jenkins-job.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 wait_seconds=0
 
-echo $3 
 ip=$3
-
 echo "URL " $1:$2 http://$ip:8080/
+
 while [[ $(curl -s -u $1:$2 -w "%{http_code}" http://$ip:8080/ -o /dev/null) != "200" ]]; do
    sleep 5
    ((wait_seconds+=5))
+   echo $wait_seconds
    if [[ $wait_seconds  -ge 30  ]]; then
       echo "Error: no se ha podido iniciar el servidor jenkins después de ${wait_seconds} segundos";
       exit 1;
