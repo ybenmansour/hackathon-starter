cb809c025b703c10984976776ebb9b9f33ff4893
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-18 11:39:43 +0200

    Update launch-jenkins-job.sh

--- scripts/launch-jenkins-job.sh
+++ scripts/launch-jenkins-job.sh
@@ -2,8 +2,11 @@
 #!/bin/bash
 wait_seconds=0
 
-echo "URL " $1:$2 http://$3:8080/
-while [[ $(curl -s -u $1:$2 -w "%{http_code}" http://$3:8080/ -o /dev/null) != "200" ]]; do
+echo $3 
+ip=$3
+
+echo "URL " $1:$2 http://$ip:8080/
+while [[ $(curl -s -u $1:$2 -w "%{http_code}" http://$ip:8080/ -o /dev/null) != "200" ]]; do
    sleep 5
    ((wait_seconds+=5))
    if [[ $wait_seconds  -ge 30  ]]; then
@@ -14,16 +17,16 @@ done
 
 jobname="tfm-nodejs-app-pipeline"
 echo "Se ejecuta el pipeline: " $jobname
-curl -sI -u $1:$2 http://$3:8080/job/$jobname/build?token=TFM_CICD 
+curl -sI -u $1:$2 http://$ip:8080/job/$jobname/build?token=TFM_CICD 
 
-while [[ $(curl -s -u $1:$2 http://$3:8080/job/tfm-nodejs-app-pipeline/lastBuild/api/json  | jq -r '.building') == true ]]; do
+while [[ $(curl -s -u $1:$2 http://$ip:8080/job/tfm-nodejs-app-pipeline/lastBuild/api/json  | jq -r '.building') == true ]]; do
    sleep 30
 done
 
-BUILD_STATUS=$(curl -k -u $1:$2 --silent http://$3:8080/job/$jobname/lastBuild/api/json | jq -r '.result')
+BUILD_STATUS=$(curl -k -u $1:$2 --silent http://$ip:8080/job/$jobname/lastBuild/api/json | jq -r '.result')
 if [[ $BUILD_STATUS == "SUCCESS" ]]; then
    echo "el job ${jobname} se ha ejecutado correctamente."
 else 
    echo "el resultado de la ejecución del job ${jobname} es ${BUILD_STATUS}"
-   echo "http://$3:8080/job/${jobname}/lastBuild/consoleText"  >> $GITHUB_STEP_SUMMARY
+   echo "http://$ip:8080/job/${jobname}/lastBuild/consoleText"  >> $GITHUB_STEP_SUMMARY
 fi
