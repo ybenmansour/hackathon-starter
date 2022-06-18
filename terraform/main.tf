2233900e1bc3d27e5645da60b2589724996b6bb4
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-18 17:54:41 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,13 @@ resource "aws_security_group" "sg_jenkins_server" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "jenkins_server" {
