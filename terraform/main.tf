5136b2a0538a53480e667ecfe63851ecf8e53e5b
ybenmansour <youssef_benmansour@hotmail.com>
2022-06-29 12:10:52 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "sg_jenkins_server" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-042e1b063a45d3a68"
+  ami           = "ami-087ab2502b7003684"
   instance_type = "t2.medium"
   vpc_security_group_ids = [aws_security_group.sg_jenkins_server.id]
   #instance_initiated_shutdown_behavior = "terminate"
